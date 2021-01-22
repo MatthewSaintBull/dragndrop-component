ff35fd3a17a59cf4605098e5529e80c9f197b62d
Matteo Santoro <mttdev1997@gmail.com>
2021-01-22 21:47:43 +0100

    centered image for upload and the label

--- dragAndDrop.js
+++ dragAndDrop.js
@@ -1,21 +1,26 @@
-const template = document.createElement('template')
+const template = document.createElement("template");
 template.innerHTML = `
 
 <link rel="stylesheet" href="./css/style.css">
-  <div class="dnd-container">
-    <div>
-      <img src="./assets/upload.svg" alt="" srcset="" width="30px">
-    </div>
+<div class="dnd-container">
+<div class="upload">
+  <div class="uploadImage">
+          <img src="./assets/upload.svg" width="64px">
   </div>
+  <div class="uploadLabel">
+          <h3 id="uploadLabel">Upload your file here</h3>
+  </div>
+</div>
+</div>
 
-`
+`;
 
 class DragAndDrop extends HTMLElement {
-  constructor(){
-    super()
-    this.attachShadow({mode:'open'})
-    this.shadowRoot.appendChild(template.content.cloneNode(true))
+  constructor() {
+    super();
+    this.attachShadow({ mode: "open" });
+    this.shadowRoot.appendChild(template.content.cloneNode(true));
   }
 }
 
-window.customElements.define('drag-n-drop', DragAndDrop)
+window.customElements.define("drag-n-drop", DragAndDrop);
